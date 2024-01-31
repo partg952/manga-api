7a412951c183eb60c246c14b2e78721432be4d78
partg952 <dankparth@gmail.com>
2024-01-31 14:01:22 +0530

    made changes after a long time

--- index.js
+++ index.js
@@ -2,35 +2,35 @@ const express = require("express");
 const app = express();
 const cheerio = require("cheerio");
 const requests = require("requests");
-
+const main_url = "https://ww7.manganelo.tv/"
 
 app.get("/",(req,res)=>{
 	const data = [];
-	requests("https://ww5.manganelo.tv/home").on("data",page=>{
+	requests(main_url+"home").on("data",page=>{
 		const $ = cheerio.load(page);
 		$("div.content-homepage-item").each(function(i){
 			console.log($("div.content-item > a").eq(i).attr("title"))
 			data.push({
 				title:$(`div.container-main-left > div.panel-content-homepage > div.content-homepage-item > div > h3 > a`).eq(i).attr("title"),
-				url:"https://ww5.manganelo.tv"+$(`div.container-main-left > div.panel-content-homepage > div.content-homepage-item > div > h3 > a`).eq(i).attr("href"),
-				image:"https://ww5.manganelo.tv/mangaimage"+$(`div.container-main-left > div.panel-content-homepage > div.content-homepage-item > div > h3 > a`).eq(i).attr("href").substr(6)+".jpg"
+				url:"https://ww7.manganelo.tv"+$(`div.container-main-left > div.panel-content-homepage > div.content-homepage-item > div > h3 > a`).eq(i).attr("href"),
+				image:"https://ww7.manganelo.tv/mangaimage"+$(`div.container-main-left > div.panel-content-homepage > div.content-homepage-item > div > h3 > a`).eq(i).attr("href").substr(6)+".jpg"
 			})
 		})
 		res.send(data);
-
+		console.log(data)
 	})
 })
 
 app.get('/search',(req,res)=>{
 	const data = [];
 	const keyword = req.query['q'];
-	requests('https://ww5.manganelo.tv/search/'+keyword).on('data',page=>{
+	requests(main_url+'search/'+keyword).on('data',page=>{
 		const $ = cheerio.load(page);
 		$("div.panel-search-story > div.search-story-item").each(function(i){
 			data.push({
 				title:$("div.search-story-item > a.item-img").eq(i).attr("title"),
-				image:"https://ww5.manganelo.tv/mangaimage"+$("div.search-story-item > a.item-img").eq(i).attr("href").substr(6)+".jpg",
-				url:"https://ww5.manganelo.tv"+$("div.search-story-item > a.item-img").eq(i).attr("href")
+				image:"https://ww7.manganelo.tv/mangaimage"+$("div.search-story-item > a.item-img").eq(i).attr("href").substr(6)+".jpg",
+				url:"https://ww7.manganelo.tv"+$("div.search-story-item > a.item-img").eq(i).attr("href")
 			})
 		})
 		res.send(data);
@@ -47,14 +47,14 @@ app.get("/info",(req,res)=>{
 		$('div.panel-story-chapter-list > ul.row-content-chapter > li').each(function(i){
 			chapters.push({
 				title:$(this).children('a').attr('title'),
-				url:"https://ww5.manganelo.tv"+$(this).children('a').attr('href')
+				url:"https://ww7.manganelo.tv"+$(this).children('a').attr('href')
 			})
 		})
 		data = {
 			chapters:chapters,
 			title:$('div.story-info-right > h1').text(),
 			summary:$('div.panel-story-info-description').text(),
-			image:"https://ww5.manganelo.tv/mangaimage"+url.replace('https://ww5.manganelo.tv/manga','')+".jpg"
+			image:"https://ww7.manganelo.tv/mangaimage"+url.replace('https://ww7.manganelo.tv/manga','')+".jpg"
 			
 		}
 	res.send(data);
